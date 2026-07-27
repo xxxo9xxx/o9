aaab53c31a04d46ecfeaf2fea519235ade9cb225
xxxo9xxx <mole0910@gmail.com>
2026-07-27 20:49:03 +0900

    Add files via upload

---
+++ cat/firebase-config.js
@@ -0,0 +1,10 @@
+// 「りっちゃん・すっくん どんぐりゲーム」と同じFirebaseプロジェクトを利用します。
+// ランキングの保存先は別コレクション（waterCatScores）なので、リス側とは混ざりません。
+window.WATER_CAT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAKJf8RNFw8Pnz4dyezwYxzKtneMTaNhYw",
+  authDomain: "ritchan-sukkun-donguri.firebaseapp.com",
+  projectId: "ritchan-sukkun-donguri",
+  storageBucket: "ritchan-sukkun-donguri.firebasestorage.app",
+  messagingSenderId: "276678740758",
+  appId: "1:276678740758:web:8a9f7321524b42a5675e37"
+};
